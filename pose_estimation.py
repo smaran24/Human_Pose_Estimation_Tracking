c6863c35503e11d83b819a8ba0c1f71365a9a484
Smaran R Satpathy <88144150+smaran24@users.noreply.github.com>
2022-08-26 18:11:49 +0530

    Add files via upload

---
+++ pose_estimation.py
@@ -0,0 +1,305 @@
+import collections
+import os
+import sys
+import time
+import math
+import datetime
+import imutils
+from models.centroidtracker import CentroidTracker
+# from object_detection import ObjectDetection
+import cv2
+import numpy as np
+from IPython import display
+from numpy.lib.stride_tricks import as_strided
+from openvino.runtime import Core
+from models.decoder import OpenPoseDecoder
+# sys.path.append("../utils")
+import models.notebook_utils as utils
+
+
+# Defining the model & weight paths
+model_path = "model/human-pose-estimation-0001.xml"
+model_weights_path = "weight/human-pose-estimation-0001.bin"
+
+# Initialize OpenVINO Runtime
+ie_core = Core()
+# Read the network and corresponding weights from a file.
+model = ie_core.read_model(model=model_path, weights=model_weights_path)
+
+# Load the model on CPU
+compiled_model = ie_core.compile_model(model=model, device_name="CPU")
+
+
+# Get the input and output names of nodes.
+input_layer = compiled_model.input(0)
+output_layers = list(compiled_model.outputs)
+
+
+# Get the input size.
+height, width = list(input_layer.shape)[2:]
+
+input_layer.any_name, [o.any_name for o in output_layers]
+decoder = OpenPoseDecoder()
+
+
+
+
+# 2D pooling in numpy 
+def pool2d(A, kernel_size, stride, padding, pool_mode="max"):
+    # Padding
+    A = np.pad(A, padding, mode="constant")
+
+    # Window view of A
+    output_shape = (
+        (A.shape[0] - kernel_size) // stride + 1,
+        (A.shape[1] - kernel_size) // stride + 1,
+    )
+    kernel_size = (kernel_size, kernel_size)
+    A_w = as_strided(
+        A,
+        shape=output_shape + kernel_size,
+        strides=(stride * A.strides[0], stride * A.strides[1]) + A.strides
+    )
+    A_w = A_w.reshape(-1, *kernel_size)
+
+    # Return the result of pooling.
+    if pool_mode == "max":
+        return A_w.max(axis=(1, 2)).reshape(output_shape)
+    elif pool_mode == "avg":
+        return A_w.mean(axis=(1, 2)).reshape(output_shape)
+    
+    
+# non maximum suppression
+def heatmap_nms(heatmaps, pooled_heatmaps):
+    return heatmaps * (heatmaps == pooled_heatmaps)
+
+
+# Get poses from results.
+def process_results(img, pafs, heatmaps):
+    # This processing comes from
+    # https://github.com/openvinotoolkit/open_model_zoo/blob/master/demos/common/python/models/open_pose.py
+    pooled_heatmaps = np.array(
+        [[pool2d(h, kernel_size=3, stride=1, padding=1, pool_mode="max") for h in heatmaps[0]]]
+    )
+    nms_heatmaps = heatmap_nms(heatmaps, pooled_heatmaps)
+
+    # Decode poses.
+    poses, scores = decoder(heatmaps, nms_heatmaps, pafs)
+    output_shape = list(compiled_model.output(index=0).partial_shape)
+    output_scale = img.shape[1] / output_shape[3].get_length(), img.shape[0] / output_shape[2].get_length()
+    # Multiply coordinates by a scaling factor.
+    poses[:, :, :2] *= output_scale
+    return poses, scores
+
+
+colors = ((255, 0, 0), (255, 0, 255), (170, 0, 255), (255, 0, 85), (255, 0, 170), (85, 255, 0),
+          (255, 170, 0), (0, 255, 0), (255, 255, 0), (0, 255, 85), (170, 255, 0), (0, 85, 255),
+          (0, 255, 170), (0, 0, 255), (0, 255, 255), (85, 0, 255), (0, 170, 255))
+
+default_skeleton = ((15, 13), (13, 11), (16, 14), (14, 12), (11, 12), (5, 11), (6, 12), (5, 6), (5, 7),
+                    (6, 8), (7, 9), (8, 10), (1, 2), (0, 1), (0, 2), (1, 3), (2, 4), (3, 5), (4, 6))
+
+
+
+def draw_poses(img, poses, point_score_threshold, skeleton=default_skeleton):
+    if poses.size == 0:
+        return img
+
+    img_limbs = np.copy(img)
+    for pose in poses:
+        points = pose[:, :2].astype(np.int32)
+        points_scores = pose[:, 2]
+        # Draw joints.
+        for i, (p, v) in enumerate(zip(points, points_scores)):
+            if v > point_score_threshold:
+                cv2.circle(img, tuple(p), 1, colors[i], 2)
+        # Draw limbs.
+        for i, j in skeleton:
+            if points_scores[i] > point_score_threshold and points_scores[j] > point_score_threshold:
+                cv2.line(img_limbs, tuple(points[i]), tuple(points[j]), color=colors[j], thickness=4)
+    cv2.addWeighted(img, 0.4, img_limbs, 0.6, 0, dst=img)
+    return img
+
+
+
+# Tracking
+
+protopath = "MobileNetSSD_deploy.prototxt"
+modelpath = "MobileNetSSD_deploy.caffemodel"
+detector = cv2.dnn.readNetFromCaffe(prototxt=protopath, caffeModel=modelpath)
+# detector.setPreferableBackend(cv2.dnn.DNN_BACKEND_INFERENCE_ENGINE)
+# detector.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+           "sofa", "train", "tvmonitor"]
+tracker = CentroidTracker(maxDisappeared=10, maxDistance=50)
+
+
+
+def non_max_suppression_fast(boxes, overlapThresh):
+    try:
+        if len(boxes) == 0:
+            return []
+
+        if boxes.dtype.kind == "i":
+            boxes = boxes.astype("float")
+
+        pick = []
+
+        x1 = boxes[:, 0]
+        y1 = boxes[:, 1]
+        x2 = boxes[:, 2]
+        y2 = boxes[:, 3]
+
+        area = (x2 - x1 + 1) * (y2 - y1 + 1)
+        idxs = np.argsort(y2)
+
+        while len(idxs) > 0:
+            last = len(idxs) - 1
+            i = idxs[last]
+            pick.append(i)
+
+            xx1 = np.maximum(x1[i], x1[idxs[:last]])
+            yy1 = np.maximum(y1[i], y1[idxs[:last]])
+            xx2 = np.minimum(x2[i], x2[idxs[:last]])
+            yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+            w = np.maximum(0, xx2 - xx1 + 1)
+            h = np.maximum(0, yy2 - yy1 + 1)
+
+            overlap = (w * h) / area[idxs[:last]]
+
+            idxs = np.delete(idxs, np.concatenate(([last],
+                                                   np.where(overlap > overlapThresh)[0])))
+
+        return boxes[pick].astype("int")
+    except Exception as e:
+        print("Exception occurred in non_max_suppression : {}".format(e))
+        
+        
+        
+        
+        
+pafs_output_key = compiled_model.output("Mconv7_stage2_L1")
+heatmaps_output_key = compiled_model.output("Mconv7_stage2_L2")
+
+cap = cv2.VideoCapture('Test_Videos/face-demographics.mp4')
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+
+fps_start_time = datetime.datetime.now()
+fps = 0
+total_frames = 0
+
+# player = utils.VideoPlayer(source=0, flip=False, fps=30, skip_first_frames=0)        # Start capturing.
+# player.start()
+
+processing_times = collections.deque()
+
+
+while True:
+    ret, frame=cap.read()
+    frame = imutils.resize(frame, width=600)
+    
+    if not ret:
+        break
+    
+    input_img = cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
+    # Create a batch of images (size = 1).
+    input_img = input_img.transpose((2,0,1))[np.newaxis, ...]
+     # Measure processing time.
+    start_time = time.time()
+    # Get results.
+    results = compiled_model([input_img])
+    stop_time = time.time()
+
+    pafs = results[pafs_output_key]
+    heatmaps = results[heatmaps_output_key]
+    # Get poses from network results.
+    poses, scores = process_results(frame, pafs, heatmaps)
+
+    # Draw poses on a frame.
+    frame = draw_poses(frame, poses, 0.1)
+
+    processing_times.append(stop_time - start_time)
+    # Use processing times from last 200 frames.
+    if len(processing_times) > 200:
+        processing_times.popleft()
+           
+    
+    
+    
+    
+    total_frames = total_frames + 1
+
+    (H, W) = frame.shape[:2]
+
+    blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
+
+    detector.setInput(blob)
+    person_detections = detector.forward()
+    rects = []
+    for i in np.arange(0, person_detections.shape[2]):
+        confidence = person_detections[0, 0, i, 2]
+        if confidence > 0.5:
+            idx = int(person_detections[0, 0, i, 1])
+
+            if CLASSES[idx] != "person":
+                continue
+
+            person_box = person_detections[0, 0, i, 3:7] * np.array([W, H, W, H])
+            (startX, startY, endX, endY) = person_box.astype("int")
+            rects.append(person_box)
+
+    boundingboxes = np.array(rects)
+    boundingboxes = boundingboxes.astype(int)
+    rects = non_max_suppression_fast(boundingboxes, 0.3)
+
+    objects = tracker.update(rects)
+    for (objectId, bbox) in objects.items():
+        x1, y1, x2, y2 = bbox
+        x1 = int(x1)
+        y1 = int(y1)
+        x2 = int(x2)
+        y2 = int(y2)
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        text = "ID: {}".format(objectId)
+        cv2.putText(frame, text, (x1, y1-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
+
+    fps_end_time = datetime.datetime.now()
+    time_diff = fps_end_time - fps_start_time
+    if time_diff.seconds == 0:
+        fps = 0.0
+    else:
+        fps = (total_frames / time_diff.seconds)
+
+    fps_text = "FPS: {:.2f}".format(fps)
+
+    cv2.putText(frame, fps_text, (5, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
+    
+    
+    
+    
+      
+    # output the frame
+    # out.write(frame) 
+      
+    # The original input frame is shown in the window 
+    cv2.imshow('Original', frame)
+  
+    # The window showing the operated video stream 
+    # cv2.imshow('frame', hsv)
+        
+  
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+        
+cap.release()
+# out.release()
+cv2.destroyAllWindows()    
